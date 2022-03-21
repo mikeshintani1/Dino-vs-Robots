f0bbcef307d561c213171fb2a2a30918dd969d25
michael shintani <mikeshintani@yahoo.com>
2022-03-21 13:48:28 -0700

    added weapon.py

---
+++ weapon.py
@@ -0,0 +1,9 @@
+
+
+class Weapon:
+    def __init__(self, name):
+        self.name = name
+        self.attack_power = 3
+        self.weapon_activated = False
+        self.weapon_charged = False
+        pass
