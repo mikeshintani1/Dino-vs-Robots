6f2d1838d47596fcfe511fa09368ed7beccb1df2
mike shintani <101653682+mikeshintani1@users.noreply.github.com>
2022-03-21 14:08:49 -0700

    Update battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -1,8 +1,7 @@
 from fleet import Fleet
 from herd import Herd
 
-from herd import Herd
-from fleet import Fleet
+
 
 
 class Battlefield:
@@ -12,26 +11,40 @@ class Battlefield:
         pass
 
     def run_game(self):
+        self.run_game = input('Press 1 to start game!: ')
         pass
 
 
 
     def display_welcome(self):
-        pass
+        print('+++++++ WELCOME TO ROBOTS VERSUS DINOSAURS +++++++++')
 
 
 
     def battle(self):
-        pass
+        if self.run_game == 1:
+            first_team = input('Choose which team will go first! 1 for Dinos! 2 for Robots! ')
+            if int(first_team) == 1:
+                input('Dino Team will go first!!')
+            elif int(first_team) == 2:
+                input('Robot Team will go first!!')
+            else:
+                print('select a different number!')
+                return
+               
+
 
 
     def dino_turn(self, dino):
-        pass
+        if self.battle == 1:
+            self.dino.attack(self.robot)
+            print(f'{self.dino.name} attacked {self.robot.name} for {self.attack_power}! ')
 
 
 
     def robo_turn(self, robot):
         pass
+
 
     def show_dino_opponent_options(self):
         pass
